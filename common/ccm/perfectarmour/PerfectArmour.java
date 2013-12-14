a89923664f88b6ec59f68765db5463cc97e8d732
captain_shadows <captain_shadows@aol.com>
2013-12-14 15:33:27 -0500

    .... things are displaing..... I need a break .......
    Signed-off-by: captain_shadows <captain_shadows@aol.com>

--- common/ccm/perfectarmour/PerfectArmour.java
+++ common/ccm/perfectarmour/PerfectArmour.java
@@ -8,11 +8,11 @@ import static ccm.perfectarmour.utils.libs.Archive.SERVER_PROXY;
 import java.io.File;
 
 import net.minecraft.item.EnumArmorMaterial;
-import net.minecraft.item.Item;
 import ccm.perfectarmour.item.CustomArmor;
 import ccm.perfectarmour.proxy.CommonProxy;
 import ccm.perfectarmour.utils.helpers.json.JsonDefaults;
 import ccm.perfectarmour.utils.helpers.json.JsonHelper;
+import ccm.perfectarmour.utils.libs.Archive;
 import cpw.mods.fml.common.Mod;
 import cpw.mods.fml.common.Mod.EventHandler;
 import cpw.mods.fml.common.Mod.Instance;
@@ -21,6 +21,7 @@ import cpw.mods.fml.common.event.FMLInitializationEvent;
 import cpw.mods.fml.common.event.FMLPostInitializationEvent;
 import cpw.mods.fml.common.event.FMLPreInitializationEvent;
 import cpw.mods.fml.common.network.NetworkMod;
+import cpw.mods.fml.common.registry.GameRegistry;
 
 @Mod(modid = MOD_ID, name = MOD_NAME, useMetadata = true)
 @NetworkMod(clientSideRequired = true, serverSideRequired = false)
@@ -48,40 +49,43 @@ public class PerfectArmour
             JsonDefaults.addDefaults(armours);
         }
         JsonHelper.read(armours);
-        
+
         // Getting our ids
-        int helmetID = Item.helmetLeather.itemID;
-        int chestID = Item.plateLeather.itemID;
-        int pantsID = Item.legsLeather.itemID;
-        int bootsID = Item.bootsLeather.itemID;
+        int helmetID = 3000/*Item.helmetLeather.itemID*/;
+        int chestID = 3001/*Item.plateLeather.itemID*/;
+        int pantsID = 3002/*Item.legsLeather.itemID*/;
+        int bootsID = 3003/*Item.bootsLeather.itemID*/;
 
         // Getting rid of Leather
-        Item.itemsList[Item.helmetLeather.itemID] = null;
-        Item.itemsList[Item.plateLeather.itemID] = null;
-        Item.itemsList[Item.legsLeather.itemID] = null;
-        Item.itemsList[Item.bootsLeather.itemID] = null;
+        //Item.itemsList[Item.helmetLeather.itemID] = null;
+        //Item.itemsList[Item.plateLeather.itemID] = null;
+        //Item.itemsList[Item.legsLeather.itemID] = null;
+        //Item.itemsList[Item.bootsLeather.itemID] = null;
         // Getting rid of Iron
-        Item.itemsList[Item.helmetIron.itemID] = null;
-        Item.itemsList[Item.plateIron.itemID] = null;
-        Item.itemsList[Item.legsIron.itemID] = null;
-        Item.itemsList[Item.bootsIron.itemID] = null;
+        //Item.itemsList[Item.helmetIron.itemID] = null;
+        //Item.itemsList[Item.plateIron.itemID] = null;
+        //Item.itemsList[Item.legsIron.itemID] = null;
+        //Item.itemsList[Item.bootsIron.itemID] = null;
         // Getting rid of Gold
-        Item.itemsList[Item.helmetGold.itemID] = null;
-        Item.itemsList[Item.plateGold.itemID] = null;
-        Item.itemsList[Item.legsGold.itemID] = null;
-        Item.itemsList[Item.bootsGold.itemID] = null;
+        //Item.itemsList[Item.helmetGold.itemID] = null;
+        //Item.itemsList[Item.plateGold.itemID] = null;
+        //Item.itemsList[Item.legsGold.itemID] = null;
+        //Item.itemsList[Item.bootsGold.itemID] = null;
         // Getting rid of Diamond
-        Item.itemsList[Item.helmetDiamond.itemID] = null;
-        Item.itemsList[Item.plateDiamond.itemID] = null;
-        Item.itemsList[Item.legsDiamond.itemID] = null;
-        Item.itemsList[Item.bootsDiamond.itemID] = null;
+        //Item.itemsList[Item.helmetDiamond.itemID] = null;
+        //Item.itemsList[Item.plateDiamond.itemID] = null;
+        //Item.itemsList[Item.legsDiamond.itemID] = null;
+        //Item.itemsList[Item.bootsDiamond.itemID] = null;
 
         helmet = new CustomArmor(helmetID, EnumArmorMaterial.IRON, 1, 0);
         chest = new CustomArmor(chestID, EnumArmorMaterial.IRON, 1, 1);
         pants = new CustomArmor(pantsID, EnumArmorMaterial.IRON, 1, 2);
         boots = new CustomArmor(bootsID, EnumArmorMaterial.IRON, 1, 3);
-        
-        
+
+        GameRegistry.registerItem(helmet, "custom.helmet", Archive.MOD_ID);
+        GameRegistry.registerItem(chest, "custom.chest", Archive.MOD_ID);
+        GameRegistry.registerItem(pants, "custom.pants", Archive.MOD_ID);
+        GameRegistry.registerItem(boots, "custom.boots", Archive.MOD_ID);
     }
 
     @EventHandler
