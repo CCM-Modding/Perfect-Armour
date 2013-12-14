0918c25f0d637941279d7bb3a58029b38eec8ed8
captain_shadows <captain_shadows@aol.com>
2013-12-13 21:43:42 -0500

    15 min commit
    Signed-off-by: captain_shadows <captain_shadows@aol.com>

--- common/ccm/perfectarmour/item/CustomArmor.java
+++ common/ccm/perfectarmour/item/CustomArmor.java
@@ -1,12 +1,39 @@
 package ccm.perfectarmour.item;
 
+import net.minecraft.entity.EntityLivingBase;
+import net.minecraft.entity.player.EntityPlayer;
 import net.minecraft.item.EnumArmorMaterial;
 import net.minecraft.item.ItemArmor;
+import net.minecraft.item.ItemStack;
+import net.minecraft.util.DamageSource;
+import net.minecraftforge.common.ISpecialArmor;
 
-public class CustomArmor extends ItemArmor
+public class CustomArmor extends ItemArmor implements ISpecialArmor
 {
     public CustomArmor(int id, EnumArmorMaterial material, int renderIndex, int type)
     {
         super(id, material, renderIndex, type);
+    }
+
+    @Override
+    public ArmorProperties getProperties(EntityLivingBase player, ItemStack armor, DamageSource source, double damage, int slot)
+    {
+        ArmorProperties armorProperties = new ArmorProperties(0, 0, 0);
+        // TODO Auto-generated method stub
+        return null;
+    }
+
+    @Override
+    public int getArmorDisplay(EntityPlayer player, ItemStack armor, int slot)
+    {
+        // TODO Auto-generated method stub
+        return 0;
+    }
+
+    @Override
+    public void damageArmor(EntityLivingBase entity, ItemStack stack, DamageSource source, int damage, int slot)
+    {
+        // TODO Auto-generated method stub
+        
     }
 }
