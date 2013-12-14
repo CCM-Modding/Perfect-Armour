4c5afb0006eb0818d095c764d2f5837b939f850c
captain_shadows <captain_shadows@aol.com>
2013-12-14 00:04:50 -0500

    15 min commit
    Signed-off-by: captain_shadows <captain_shadows@aol.com>

--- common/ccm/perfectarmour/item/CustomArmor.java
+++ common/ccm/perfectarmour/item/CustomArmor.java
@@ -1,5 +1,8 @@
 package ccm.perfectarmour.item;
 
+import java.util.List;
+
+import net.minecraft.creativetab.CreativeTabs;
 import net.minecraft.entity.EntityLivingBase;
 import net.minecraft.entity.player.EntityPlayer;
 import net.minecraft.item.EnumArmorMaterial;
@@ -13,22 +16,44 @@ public class CustomArmor extends ItemArmor implements ISpecialArmor
     public CustomArmor(int id, EnumArmorMaterial material, int renderIndex, int type)
     {
         super(id, material, renderIndex, type);
+        setHasSubtypes(true);
+        setMaxDamage(Short.MAX_VALUE);
+    }
+
+    @Override
+    public int getDisplayDamage(ItemStack stack)
+    {// Display the right damage, NBT ITEM.DAMAGE
+        return super.getDisplayDamage(stack);
+    }
+
+    @Override
+    public String getItemStackDisplayName(ItemStack stack)
+    {// Display the name
+        return super.getItemStackDisplayName(stack);
+    }
+    
+    @Override
+    public void getSubItems(int id, CreativeTabs tab, List list)
+    {// Add all registered types
+        super.getSubItems(id, tab, list);
     }
 
     @Override
     public ArmorProperties getProperties(EntityLivingBase player, ItemStack armor, DamageSource source, double damage, int slot)
-    {
+    {// take armor and change into armor properties
         ArmorProperties armorProperties = new ArmorProperties(0, 0, 0);
         return null;
     }
 
     @Override
     public int getArmorDisplay(EntityPlayer player, ItemStack armor, int slot)
-    {
+    {// change to max protection
         return 0;
     }
-
+    
     @Override
     public void damageArmor(EntityLivingBase entity, ItemStack stack, DamageSource source, int damage, int slot)
-    {}
+    {// Increase? or reduce? NBT ITEM.DAMAGE
+        
+    }
 }
