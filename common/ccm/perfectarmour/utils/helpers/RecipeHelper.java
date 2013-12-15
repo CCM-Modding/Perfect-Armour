c64b413224dc2f6422b891deeb3915f9677d191e
captain_shadows <captain_shadows@aol.com>
2013-12-14 22:25:32 -0500

    15 min commit
    Signed-off-by: captain_shadows <captain_shadows@aol.com>

---
+++ common/ccm/perfectarmour/utils/helpers/RecipeHelper.java
@@ -0,0 +1,54 @@
+package ccm.perfectarmour.utils.helpers;
+
+import net.minecraft.item.ItemStack;
+import net.minecraft.item.crafting.IRecipe;
+import net.minecraft.nbt.NBTTagCompound;
+import net.minecraftforge.oredict.ShapedOreRecipe;
+
+public class RecipeHelper
+{
+    public static IRecipe nbtRecipe(NBTTagCompound nbt)
+    {
+        IRecipe recipe = null;
+        
+        Object[] inputs;
+        for(nbt.getTags())
+        
+        return recipe;
+    }
+
+    private static ItemStack getItemStack(final String itemID)
+    {
+        int id = 0;
+        int meta = 0;
+
+        if (itemID.contains(":"))
+        {// Decompose String into (item ID, Meta) pairs
+            final String[] tmp = itemID.split(":");
+            if ((tmp != null) && (tmp.length > 0))
+            {
+                try
+                {
+                    id = Integer.parseInt(tmp[0]);
+                    if (tmp.length > 1)
+                    {
+                        try
+                        {
+                            meta = Integer.parseInt(tmp[1]);
+                        } catch (final Exception ex)
+                        {
+                            meta = 0;
+                        }
+                    }
+                } catch (final Exception ex)
+                {
+                    id = 0;
+                }
+            }
+        } else
+        {//Its only an id
+            id = Integer.parseInt(itemID);
+        }
+        return new ItemStack(id, 1, meta);
+    }
+}
